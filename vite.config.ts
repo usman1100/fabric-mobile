5f6d02480d99a97c8705d386c6a835ea7dd515eb
usman1100 <usmanahmed1100@gmail.com>
2024-11-01 19:09:33 +0500

    update pwa refresh policy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
         name: "Fabric Mobile",
