b6094261da8d1cbe37953afd781220f733154de4
usman1100 <usmanahmed1100@gmail.com>
2024-11-12 19:33:52 +0500

    add back button

--- src/components/shared/TopNavbar.tsx
+++ src/components/shared/TopNavbar.tsx
@@ -1,9 +1,25 @@
-import { Stack, Typography } from "@mui/material";
+import { IconButton, Stack } from "@mui/material";
+import ArrowBack from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const onClick = () => {
+    navigate(-1);
+  }
+
+  return (
+    <IconButton onClick={onClick}>
+      <ArrowBack />
+    </IconButton>
+  );
+};
 
 export const TopNavbar = () => {
   return (
-    <Stack direction={"row"} px={2}>
-      <Typography variant="body2"> Fabric Mobile</Typography>
+    <Stack direction={"row"} px={2} py={1} alignItems="center">
+      <BackButton />
     </Stack>
   );
 };
